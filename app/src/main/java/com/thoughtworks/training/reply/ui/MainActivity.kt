2a85cd8f79a7b431c0345e6cc55af8b7cf92335a
liululu123456 <626463942@qq.com>
2024-04-15 23:32:18 +0800

    feat: switch theme

--- app/src/main/java/com/thoughtworks/training/reply/ui/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import com.thoughtworks.training.reply.ui.theme.ContrastAwareReplyTheme
 
 class MainActivity : ComponentActivity() {
